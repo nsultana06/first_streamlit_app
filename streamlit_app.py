1f32911ab6855d5a9fd5a9ff559d93528bf06296
Nigar Sultana <120883398+nsultana06@users.noreply.github.com>
2022-12-17 23:18:55 -0600

    Update streamlit_app.py
    ' in Mom's gave an error.  Changed back to the original Header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('🥣 🥗 🐔 🥑🍞')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
